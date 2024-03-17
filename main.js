5dca2a03260ac359d276873403c966b63f80a752
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-03-17 01:29:24 -0400

    Part-I completed

---
+++ main.js
@@ -0,0 +1,20 @@
+// Part-----II: Creating a Menu Bar
+
+
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
